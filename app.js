02b8a095c2c8d65c26744fb7755cb607e2dcc827
Matthew Short <short.matthew.f@gmail.com>
2018-10-05 12:29:37 -0400

    gh-pages

---
+++ app.js
@@ -0,0 +1,12 @@
+const setBackgroundElGradient = (percent, MIN_DEG = 150, MAX_DEG = 210) => {
+	const backgroundEl = document.querySelector('.background');
+	const degree = (MAX_DEG - MIN_DEG) * percent + MIN_DEG;
+	backgroundEl.style.backgroundImage = `linear-gradient(${degree}deg, #ff0, #ff0 50%, #0ff 50%)`;
+};
+
+window.addEventListener('scroll', (event) => {
+	const percentScrolled = window.pageYOffset / (document.body.scrollHeight - window.innerHeight);
+	setBackgroundElGradient(percentScrolled, 30, 330);
+});
+
+setBackgroundElGradient(0, -30, 330);
